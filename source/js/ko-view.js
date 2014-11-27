22503124f8f0a91984ff386b78f9e61671a9ca72
lucidtech <chris@safemonk.com>
2014-11-27 09:45:22 -0500

    Update ko-view.js

--- source/js/ko-view.js
+++ source/js/ko-view.js
@@ -43,7 +43,7 @@ javascript:
   modKey,
   lastKvovIdGiven = 0,
 
-  context = ko.contextFor(document.body),
+  context = ko.contextFor(document.getElementsByTagName('firstrun')[0]),
 
   showText          = "Show",
   hideText          = "Hide",
